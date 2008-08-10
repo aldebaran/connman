e53e3eb4fae306bfb6bf26bf57c3a9530841487d
Marcel Holtmann <marcel@holtmann.org>
2008-08-10 10:26:25 +0200

    Remove deprecated device API

--- src/manager.c
+++ src/manager.c
@@ -123,38 +123,11 @@ static DBusMessage *list_elements(DBusConnection *conn,
 	return reply;
 }
 
-static DBusMessage *list_devices(DBusConnection *conn,
-					DBusMessage *msg, void *data)
-{
-	DBusMessage *reply;
-	DBusMessageIter array, iter;
-
-	DBG("conn %p", conn);
-
-	reply = dbus_message_new_method_return(msg);
-	if (reply == NULL)
-		return NULL;
-
-	dbus_message_iter_init_append(reply, &array);
-
-	dbus_message_iter_open_container(&array, DBUS_TYPE_ARRAY,
-				DBUS_TYPE_OBJECT_PATH_AS_STRING, &iter);
-
-	__connman_element_list(CONNMAN_ELEMENT_TYPE_DEVICE, &iter);
-
-	dbus_message_iter_close_container(&array, &iter);
-
-	return reply;
-}
-
 static GDBusMethodTable manager_methods[] = {
-	{ "RegisterAgent",   "o", "", register_agent   },
-	{ "UnregisterAgent", "o", "", unregister_agent },
-
-	{ "ListProfiles", "", "ao", list_profiles },
-
-	{ "ListElements", "", "ao", list_elements },
-	{ "ListDevices",  "", "ao", list_devices  },
+	{ "RegisterAgent",   "o", "",   register_agent   },
+	{ "UnregisterAgent", "o", "",   unregister_agent },
+	{ "ListProfiles",    "",  "ao", list_profiles    },
+	{ "ListElements",    "",  "ao", list_elements    },
 	{ },
 };
 
@@ -162,8 +135,6 @@ static GDBusSignalTable manager_signals[] = {
 	{ "ElementAdded",   "o" },
 	{ "ElementUpdated", "o" },
 	{ "ElementRemoved", "o" },
-	{ "DeviceAdded",    "o" },
-	{ "DeviceRemoved",  "o" },
 	{ },
 };
 
