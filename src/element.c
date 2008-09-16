ad94c021ad5df8b199f1d74510ed19e61ca2899a
Marcel Holtmann <marcel@holtmann.org>
2008-09-16 19:58:14 +0200

    Add debug message when device is ignored

--- src/element.c
+++ src/element.c
@@ -957,8 +957,10 @@ int connman_element_register(struct connman_element *element,
 	DBG("element %p name %s parent %p", element, element->name, parent);
 
 	if (device_filter && element->type == CONNMAN_ELEMENT_TYPE_DEVICE) {
-		if (g_str_equal(device_filter, element->name) == FALSE)
+		if (g_str_equal(device_filter, element->name) == FALSE) {
+			DBG("ignoring %s device", element->name);
 			return -EPERM;
+		}
 	}
 
 	if (connman_element_ref(element) == NULL)
