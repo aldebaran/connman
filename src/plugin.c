e3e56199f01eda60b6f2d54d4133137ef546314b
Marcel Holtmann <marcel@holtmann.org>
2008-10-18 18:06:18 +0200

    Fix memory leak when plugin init fails

--- src/plugin.c
+++ src/plugin.c
@@ -47,8 +47,10 @@ static gboolean add_plugin(void *handle, struct connman_plugin_desc *desc)
 	plugin->handle = handle;
 	plugin->desc = desc;
 
-	if (desc->init() < 0)
+	if (desc->init() < 0) {
+		g_free(plugin);
 		return FALSE;
+	}
 
 	plugins = g_slist_append(plugins, plugin);
 
