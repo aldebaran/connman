cd762b0091f6fda81e2a49b2e339abd1a446bc00
Marcel Holtmann <marcel@holtmann.org>
2008-12-09 01:55:26 +0100

    Include signal strength property

--- plugins/wifi.c
+++ plugins/wifi.c
@@ -239,8 +239,8 @@ static void scan_result(struct connman_element *parent,
 
 		strength = network->quality;
 
-		//connman_element_add_static_property(element, "WiFi.Strength",
-		//				DBUS_TYPE_BYTE, &strength);
+		connman_element_add_static_property(element, "WiFi.Strength",
+						DBUS_TYPE_BYTE, &strength);
 
 		//connman_element_add_static_property(element, "WiFi.Noise",
 		//			DBUS_TYPE_INT32, &network->noise);
