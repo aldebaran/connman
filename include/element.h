366dc1fdece8aa53344d054d34fa8e698f405770
Marcel Holtmann <marcel@holtmann.org>
2008-12-17 15:35:17 +0100

    Remove duplicate function prototype

--- include/element.h
+++ include/element.h
@@ -151,9 +151,6 @@ extern void connman_element_update(struct connman_element *element);
 extern int connman_element_set_enabled(struct connman_element *element,
 							gboolean enabled);
 
-extern int connman_element_set_enabled(struct connman_element *element,
-							gboolean enabled);
-
 static inline void *connman_element_get_data(struct connman_element *element)
 {
 	return element->driver_data;
