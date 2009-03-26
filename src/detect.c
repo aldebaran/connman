af6de21147d970e9d34fd1e4176bee96e7b1c6b4
Marcel Holtmann <marcel@holtmann.org>
2009-03-26 09:10:17 +0100

    Ignore VirtualBox network devices

--- src/detect.c
+++ src/detect.c
@@ -209,6 +209,8 @@ static void detect_newlink(unsigned short type, int index,
 			devtype = CONNMAN_DEVICE_TYPE_UNKNOWN;
 		else if (g_str_has_prefix(devname, "vmnet") == TRUE)
 			devtype = CONNMAN_DEVICE_TYPE_UNKNOWN;
+		else if (g_str_has_prefix(devname, "vboxnet") == TRUE)
+			devtype = CONNMAN_DEVICE_TYPE_UNKNOWN;
 		else if (stat(bridge_path, &st) == 0 && (st.st_mode & S_IFDIR))
 			devtype = CONNMAN_DEVICE_TYPE_UNKNOWN;
 		else if (stat(wimax_path, &st) == 0 && (st.st_mode & S_IFDIR))
