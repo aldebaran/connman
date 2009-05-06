b374f6d47e4953aa20795ac6b5ba2709dab382cf
Marcel Holtmann <marcel@holtmann.org>
2009-05-05 17:55:52 -0700

    Add printf format checking attribute

--- include/log.h
+++ include/log.h
@@ -32,9 +32,12 @@ extern "C" {
  * @short_description: Functions for logging error and debug information
  */
 
-extern void connman_info(const char *format, ...);
-extern void connman_error(const char *format, ...);
-extern void connman_debug(const char *format, ...);
+extern void connman_info(const char *format, ...)
+				__attribute__((format(printf, 1, 2)));
+extern void connman_error(const char *format, ...)
+				__attribute__((format(printf, 1, 2)));
+extern void connman_debug(const char *format, ...)
+				__attribute__((format(printf, 1, 2)));
 
 /**
  * DBG:
