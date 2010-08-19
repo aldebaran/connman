9dd8e492f5fef44edb210ed41eed200a20985f07
Marcel Holtmann <marcel@holtmann.org>
2010-08-19 19:38:01 +0200

    Remove wrong comma from D-Bus error message helper

--- src/error.c
+++ src/error.c
@@ -180,6 +180,6 @@ DBusMessage *__connman_error_invalid_service(DBusMessage *msg)
 
 DBusMessage *__connman_error_invalid_property(DBusMessage *msg)
 {
-	return g_dbus_create_error(msg, CONNMAN_ERROR_INTERFACE,
+	return g_dbus_create_error(msg, CONNMAN_ERROR_INTERFACE
 				".InvalidProperty", "Invalid property");
 }
