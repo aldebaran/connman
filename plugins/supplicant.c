881ab6a3fc4b74efa0c9a618c48ac90a7a56ed23
Samuel Ortiz <sameo@linux.intel.com>
2010-09-30 16:33:40 +0200

    Set WiFi task scanning flag when receiving a Scanning event
    wpa_supplicant can start scanning without ConnMan telling it to do so (in
    the roaming case for example), and thus the task scanning state should be
    updated accordingly in that case.
    Without this fix the device network list is not refreshed appropriately.

--- plugins/supplicant.c
+++ plugins/supplicant.c
@@ -2224,6 +2224,9 @@ static void scanning(struct supplicant_task *task, DBusMessage *msg)
 
 	connman_info("%s scanning %s", task->ifname,
 				scanning == TRUE ? "started" : "finished");
+
+	if (scanning == TRUE)
+		task->scanning = TRUE;
 }
 
 static gboolean delayed_scan(gpointer user_data)
