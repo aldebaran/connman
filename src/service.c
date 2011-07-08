d5fcad5c684b81db393899ab5f0927299d4fb3ec
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2011-07-08 19:31:03 +0300

    service: preventing to call ready state more than once.

--- src/service.c
+++ src/service.c
@@ -363,7 +363,8 @@ static enum connman_service_state combine_state(
 	}
 
 	if (state_a == CONNMAN_SERVICE_STATE_READY) {
-		if (state_b == CONNMAN_SERVICE_STATE_ONLINE)
+		if (state_b == CONNMAN_SERVICE_STATE_ONLINE ||
+				state_b == CONNMAN_SERVICE_STATE_DISCONNECT)
 			result = state_b;
 		else
 			result = state_a;
@@ -371,7 +372,8 @@ static enum connman_service_state combine_state(
 	}
 
 	if (state_b == CONNMAN_SERVICE_STATE_READY) {
-		if (state_a == CONNMAN_SERVICE_STATE_ONLINE)
+		if (state_a == CONNMAN_SERVICE_STATE_ONLINE ||
+				state_a == CONNMAN_SERVICE_STATE_DISCONNECT)
 			result = state_a;
 		else
 			result = state_b;
@@ -389,12 +391,12 @@ static enum connman_service_state combine_state(
 	}
 
 	if (state_a == CONNMAN_SERVICE_STATE_DISCONNECT) {
-		result = state_b;
+		result = state_a;
 		goto done;
 	}
 
 	if (state_b == CONNMAN_SERVICE_STATE_DISCONNECT) {
-		result = state_a;
+		result = state_b;
 		goto done;
 	}
 
