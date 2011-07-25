6599246373378e0cd513b691300d52f1ac3a44df
Daniel Wagner <daniel.wagner@bmw-carit.de>
2011-07-25 16:24:36 +0200

    network: Fix double free in network_destruct()

--- src/network.c
+++ src/network.c
@@ -329,7 +329,6 @@ static void network_destruct(struct connman_network *network)
 	g_free(network->node);
 	g_free(network->name);
 	g_free(network->identifier);
-	g_free(network->path);
 
 	network->device = NULL;
 
