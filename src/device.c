ae38f8e402fccd5dea50b098f35c97a532cd17e9
Daniel Wagner <daniel.wagner@bmw-carit.de>
2011-07-25 16:24:27 +0200

    device: Fix a inbalance of ref/unref of network objects
    The destroy callback of the hash table (device->networks)
    will unref and unregister the network object.

--- src/device.c
+++ src/device.c
@@ -1138,15 +1138,12 @@ void __connman_device_set_network(struct connman_device *device,
 	if (device->network == network)
 		return;
 
-	if (device->network != NULL)
-		connman_network_unref(device->network);
-
 	if (network != NULL) {
 		name = connman_network_get_string(network, "Name");
 		g_free(device->last_network);
 		device->last_network = g_strdup(name);
 
-		device->network = connman_network_ref(network);
+		device->network = network;
 	} else {
 		g_free(device->last_network);
 		device->last_network = NULL;
