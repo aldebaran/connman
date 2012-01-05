af1e251f8f1e0c6c50e25458793c8d2520e86f52
Alok Barsode <alok.barsode@linux.intel.com>
2012-01-05 14:41:13 +0200

    clock: Emit property changed signal when time property is channged.
    Fixes BMC#23740.

--- src/clock.c
+++ src/clock.c
@@ -190,6 +190,10 @@ static DBusMessage *set_property(DBusConnection *conn,
 
 		if (settimeofday(&tv, NULL) < 0)
 			return __connman_error_invalid_arguments(msg);
+
+		connman_dbus_property_changed_basic(CONNMAN_MANAGER_PATH,
+				CONNMAN_CLOCK_INTERFACE, "Time",
+				DBUS_TYPE_UINT64, &newval);
 	} else if (g_str_equal(name, "TimeUpdates") == TRUE) {
 		const char *strval;
 		enum time_updates newval;
