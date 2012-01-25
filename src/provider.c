080e04b077f4c54da9045c8b4e874b92f98e9ceb
Patrik Flykt <patrik.flykt@linux.intel.com>
2012-01-25 13:49:45 +0200

    provider: Don't remove a provider on error
    When a provider indicates error, set the corresponding
    service error but don't remove the provider.

--- src/provider.c
+++ src/provider.c
@@ -439,8 +439,6 @@ int connman_provider_indicate_error(struct connman_provider *provider,
 					enum connman_provider_error error)
 {
 	enum connman_service_error service_error;
-	const char *path;
-	int ret;
 
 	switch (error) {
 	case CONNMAN_PROVIDER_ERROR_LOGIN_FAILED:
@@ -457,12 +455,8 @@ int connman_provider_indicate_error(struct connman_provider *provider,
 		break;
 	}
 
-	ret = __connman_service_indicate_error(provider->vpn_service,
+	return __connman_service_indicate_error(provider->vpn_service,
 							service_error);
-	path = __connman_service_get_path(provider->vpn_service);
-	__connman_provider_remove(path);
-
-	return ret;
 }
 
 static void unregister_provider(gpointer data)
