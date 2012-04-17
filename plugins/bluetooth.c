1a9faff827c73959eeb4a35f898a591bbf6f5004
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2012-04-17 13:54:20 +0300

    bluetooth: use g_hash_table_replace instead of insert function

--- plugins/bluetooth.c
+++ plugins/bluetooth.c
@@ -446,7 +446,7 @@ static void network_properties_reply(DBusPendingCall *call, void *user_data)
 
 	connman_network_set_group(network, ident);
 
-	g_hash_table_insert(bluetooth_networks, g_strdup(path), network);
+	g_hash_table_replace(bluetooth_networks, g_strdup(path), network);
 
 done:
 	dbus_message_unref(reply);
