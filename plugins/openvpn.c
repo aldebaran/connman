2b8ef6f34662ed024d5fa015442e113fccbd6663
Emil Renner Berthing <esmil@mailme.dk>
2012-10-23 13:29:24 +0300

    openvpn: Support '--tls-auth <file>' with optional '<direction>' argument
    Connman              OpenVPN
    
    OpenVPN.TLSAuth      --tls-auth <file> [<direction>]
    OpenVPN.TLSAuthDir   optional <direction> argument to '--tls-auth'
    
    Since 'OpenVPN.TLSAuthDir' contains the optional <direction> argument for
    'OpenVPN.TLSAuth', handle them separately. The special handling is
    identified by setting openvpn command line argument to NULL.

--- plugins/openvpn.c
+++ plugins/openvpn.c
@@ -62,6 +62,8 @@ struct {
 	{ "OpenVPN.AskPass", "--askpass", 1 },
 	{ "OpenVPN.AuthNoCache", "--auth-nocache", 0 },
 	{ "OpenVPN.TLSRemote", "--tls-remote", 1 },
+	{ "OpenVPN.TLSAuth", NULL, 1 },
+	{ "OpenVPN.TLSAuthDir", NULL, 1 },
 	{ "OpenVPN.Cipher", "--cipher", 1 },
 	{ "OpenVPN.Auth", "--auth", 1 },
 	{ "OpenVPN.CompLZO", "--comp-lzo", 0 },
@@ -206,6 +208,9 @@ static int task_append_config_data(struct connman_provider *provider,
 	int i;
 
 	for (i = 0; i < (int)ARRAY_SIZE(ov_options); i++) {
+		if (ov_options[i].ov_opt == NULL)
+			continue;
+
 		option = connman_provider_get_string(provider,
 					ov_options[i].cm_opt);
 		if (option == NULL)
@@ -234,6 +239,15 @@ static int ov_connect(struct connman_provider *provider,
 	}
 
 	task_append_config_data(provider, task);
+
+	option = connman_provider_get_string(provider, "OpenVPN.TLSAuth");
+	if (option != NULL) {
+		connman_task_add_argument(task, "--tls-auth", option);
+		option = connman_provider_get_string(provider,
+				"OpenVPN.TLSAuthDir");
+		if (option != NULL)
+			connman_task_add_argument(task, option, NULL);
+	}
 
 	connman_task_add_argument(task, "--syslog", NULL);
 
