27e37a50f35cc54c266cbd37e32dadbf3016e5e8
Marcel Holtmann <marcel@holtmann.org>
2012-12-11 12:07:08 +0100

    client: Fix the mangled copyright statement
    In addition relax it to GPLv2 or later to make linking against
    newer versions of readline library possible.

--- client/services.h
+++ client/services.h
@@ -5,8 +5,9 @@
  *  Copyright (C) 2012  Intel Corporation. All rights reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,6 +19,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
+
 #ifndef __CLIENT_SERVICES_H
 #define __CLIENT_SERVICES_H
 
