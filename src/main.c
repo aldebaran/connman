31005cf64198049c265b18b82b1d34bc9cc024bc
Daniel Wagner <daniel.wagner@bmw-carit.de>
2013-07-02 15:45:25 +0200

    build: Remove unused --enable-threads configure switch
    A leftover from the WiMAX plugin which was removed by
    bea636b41 build: Remove WiMAX from configuration options

--- src/main.c
+++ src/main.c
@@ -573,11 +573,6 @@ int main(int argc, char *argv[])
 	DBusError err;
 	guint signal;
 
-#ifdef NEED_THREADS
-	if (g_thread_supported() == FALSE)
-		g_thread_init(NULL);
-#endif
-
 	context = g_option_context_new(NULL);
 	g_option_context_add_main_entries(context, options, NULL);
 
@@ -619,13 +614,6 @@ int main(int argc, char *argv[])
 	umask(0077);
 
 	main_loop = g_main_loop_new(NULL, FALSE);
-
-#ifdef NEED_THREADS
-	if (dbus_threads_init_default() == FALSE) {
-		fprintf(stderr, "Can't init usage of threads\n");
-		exit(1);
-	}
-#endif
 
 	signal = setup_signalfd();
 
