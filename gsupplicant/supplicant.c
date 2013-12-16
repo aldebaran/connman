aldebaran/connman

commit 363393cfb1a5f95f8892f40662486c87b80d0091

gsupplicant: Fix empty string test
The address of an array is never NULL, so the comparison will
always evaluate the same way. Instead test for the length
of the SSID.

Reported by coverity.

gsupplicant/supplicant.c
@@ -735,7 +735,7 @@ const void *g_supplicant_interface_get_wps_ssid(GSupplicantInterface *interface,
 	if (!ssid_len)
 		return NULL;
 
-	if (!interface || !interface->wps_cred.ssid) {
+	if (!interface || interface->wps_cred.ssid_len == 0) {
 		*ssid_len = 0;
 		return NULL;
 	}
@@ -853,7 +853,7 @@ const char *g_supplicant_network_get_security(GSupplicantNetwork *network)
 const void *g_supplicant_network_get_ssid(GSupplicantNetwork *network,
 						unsigned int *ssid_len)
 {
-	if (!network || !network->ssid) {
+	if (!network || network->ssid_len == 0) {
 		*ssid_len = 0;
 		return NULL;
 	}

commit 91913c40054d9c55c6d38f0c626a78bdbcf4279a

gsupplicant: A network ssid of length 0 is valid, it's an hidden one
In case of an hidden AP around, its ssid has a length of 0, but it is
obviously a valid network anyway.

Fixes such regression from commit 363393cfb1a5f95f8892f40662486c87b80d0091

Reported by Sameer Naik

gsupplicant/supplicant.c
@@ -853,7 +853,7 @@ const char *g_supplicant_network_get_security(GSupplicantNetwork *network)
 const void *g_supplicant_network_get_ssid(GSupplicantNetwork *network,
 						unsigned int *ssid_len)
 {
-	if (!network || network->ssid_len == 0) {
+	if (!network) {
 		*ssid_len = 0;
 		return NULL;
 	}