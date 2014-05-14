26cc7f68b85cbb02a095bcaa408ad516ff634ee5
Hannu Mallat <hannu.mallat@jollamobile.com>
2014-05-14 09:22:22 +0300

    agent: Check for NULL parameter
    Check for reply parameter being NULL before using it.

--- src/agent-connman.c
+++ src/agent-connman.c
@@ -354,6 +354,9 @@ static void request_input_login_reply(DBusMessage *reply, void *user_data)
 	char *key;
 	DBusMessageIter iter, dict;
 
+	if (!reply)
+		goto out;
+
 	if (dbus_message_get_type(reply) == DBUS_MESSAGE_TYPE_ERROR) {
 		error = dbus_message_get_error_name(reply);
 		goto done;
@@ -401,6 +404,8 @@ done:
 					username, password,
 					FALSE, NULL, error,
 					username_password_reply->user_data);
+
+out:
 	g_free(username_password_reply);
 }
 
