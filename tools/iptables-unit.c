a991fa4bb5a63dc926d2cbee8f3eda9120a73690
Daniel Wagner <daniel.wagner@bmw-carit.de>
2014-06-10 11:07:30 +0200

    tools: Fix copyright statement
    The copyright statement for my company is not consistent or even
    wrongly spelled.
    
    s/All rights reserved//
    s/CarIT/Car IT/
    s/BWM/BMW/
    s/GbmH/GmbH/

--- tools/iptables-unit.c
+++ tools/iptables-unit.c
@@ -2,7 +2,7 @@
  *
  *  Connection Manager
  *
- *  Copyright (C) 2013  BWM CarIT GmbH. All rights reserved.
+ *  Copyright (C) 2013-2014  BMW Car IT GmbH.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
