94d8d9c63cacf1484b02036935cd976b84b5e896
Erik Larsson <erik@ortogonal.com>
2014-11-12 11:18:41 +0100

    agent-connman: Fix crash due to NULL D-Bus reply message
    When an outstanding request browser call is cancelled, the D-Bus reply
    will be set to NULL. Check the reply message before acting on it.

--- src/agent-connman.c
+++ src/agent-connman.c
@@ -582,6 +582,9 @@ static void request_browser_reply(DBusMessage *reply, void *user_data)
 	bool result = false;
 	const char *error = NULL;
 
+	if (!reply)
+		goto done;
+
 	if (dbus_message_get_type(reply) == DBUS_MESSAGE_TYPE_ERROR) {
 		error = dbus_message_get_error_name(reply);
 		goto done;
