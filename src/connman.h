f0143b40b1c37362096291f4f65da556a53ead7c
Pasi Sjöholm <pasi.sjoholm@jollamobile.com>
2015-03-03 23:01:41 +0200

    ipconfig: Modify __connman_ipconfig_newaddr() to return success/fail
    __connman_ipconfig_newaddr() needs to return success / fail for
    rtln.c:process_newaddr() so that we know if it's necessary to run
    __connman_resolver_redo_servers().

--- src/connman.h
+++ src/connman.h
@@ -338,7 +338,7 @@ void __connman_ipconfig_newlink(int index, unsigned short type,
 							unsigned short mtu,
 						struct rtnl_link_stats *stats);
 void __connman_ipconfig_dellink(int index, struct rtnl_link_stats *stats);
-void __connman_ipconfig_newaddr(int index, int family, const char *label,
+int __connman_ipconfig_newaddr(int index, int family, const char *label,
 				unsigned char prefixlen, const char *address);
 void __connman_ipconfig_deladdr(int index, int family, const char *label,
 				unsigned char prefixlen, const char *address);
--- src/ipconfig.c
+++ src/ipconfig.c
@@ -638,7 +638,7 @@ static inline gint check_duplicate_address(gconstpointer a, gconstpointer b)
 	return g_strcmp0(addr1->local, addr2->local);
 }
 
-void __connman_ipconfig_newaddr(int index, int family, const char *label,
+int __connman_ipconfig_newaddr(int index, int family, const char *label,
 				unsigned char prefixlen, const char *address)
 {
 	struct connman_ipdevice *ipdevice;
@@ -651,11 +651,11 @@ void __connman_ipconfig_newaddr(int index, int family, const char *label,
 
 	ipdevice = g_hash_table_lookup(ipdevice_hash, GINT_TO_POINTER(index));
 	if (!ipdevice)
-		return;
+		return -ENXIO;
 
 	ipaddress = connman_ipaddress_alloc(family);
 	if (!ipaddress)
-		return;
+		return -ENOMEM;
 
 	ipaddress->prefixlen = prefixlen;
 	ipaddress->local = g_strdup(address);
@@ -663,7 +663,7 @@ void __connman_ipconfig_newaddr(int index, int family, const char *label,
 	if (g_slist_find_custom(ipdevice->address_list, ipaddress,
 					check_duplicate_address)) {
 		connman_ipaddress_free(ipaddress);
-		return;
+		return -EALREADY;
 	}
 
 	if (family == AF_INET)
@@ -671,7 +671,7 @@ void __connman_ipconfig_newaddr(int index, int family, const char *label,
 	else if (family == AF_INET6)
 		type = CONNMAN_IPCONFIG_TYPE_IPV6;
 	else
-		return;
+		return -EINVAL;
 
 	ipdevice->address_list = g_slist_prepend(ipdevice->address_list,
 								ipaddress);
@@ -715,6 +715,7 @@ void __connman_ipconfig_newaddr(int index, int family, const char *label,
 
 out:
 	g_free(ifname);
+	return 0;
 }
 
 void __connman_ipconfig_deladdr(int index, int family, const char *label,
