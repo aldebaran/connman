22d4605ae647db5d90c88d9f66164c66867334ed
Patrik Flykt <patrik.flykt@linux.intel.com>
2015-08-12 12:27:56 +0300

    service: Do not send PropertyChanged signals before service is announced
    Send no service PropertyChanged signals before the service is announced
    in ServicesChanged. Move the ServicesChanged adding right after
    registering a service to D-Bus, as any ipconfig state updates in between
    caused service PropertyChanged signals to be sent as the service was
    missing from the new services hash for ServicesChanged signal.

--- src/service.c
+++ src/service.c
@@ -6783,6 +6783,7 @@ struct connman_service * __connman_service_create_from_network(struct connman_ne
 		service->ipconfig_ipv6 = create_ip6config(service, index);
 
 	service_register(service);
+	service_schedule_added(service);
 
 	if (service->favorite) {
 		device = connman_network_get_device(service->network);
@@ -6815,7 +6816,6 @@ struct connman_service * __connman_service_create_from_network(struct connman_ne
 	}
 
 	__connman_notifier_service_add(service, service->name);
-	service_schedule_added(service);
 
 	return service;
 }
