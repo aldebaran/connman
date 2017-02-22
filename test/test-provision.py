bbab9ed944866c39c8447dcdb9d716e42e55d275
Pierre Le Magourou <plemagourou@aldebaran.com>
2017-02-22 17:37:34 +0100

    test: Update provision test to reflect changes on 'Set' method.
    As the parameter of the 'Set' function is now a dict of string and
    variant, the dict values need to be put in DBus variants.

--- test/test-provision.py
+++ test/test-provision.py
@@ -6,11 +6,11 @@ bus = dbus.SystemBus()
 provision = dbus.Interface(bus.get_object("net.connman", "/"),
 					"net.connman.Provision")
 creds = dict()
-creds["Name"] = "Memory"
-creds["EAP"] = "peap"
-creds["Phase2"] = "MSCHAPv2"
-creds["Identity"] = "John"
-creds["Passphrase"] = "Doe"
+creds["Name"] = dbus.String("Memory", variant_level=1)
+creds["EAP"] = dbus.String("peap", variant_level=1)
+creds["Phase2"] = dbus.String("MSCHAPv2", variant_level=1)
+creds["Identity"] = dbus.String("John", variant_level=1)
+creds["Passphrase"] = dbus.String("Doe", variant_level=1)
 
 provision.Set(creds)
 
